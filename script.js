ca3ba43795850588f7aefce237c600028a7f183c
Margaret <wanjirundirangu43@gmail.com>
2022-06-06 08:21:22 +0300

    index.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Spoke Vocational Training Centre";
 document.querySelector("body").appendChild(h2);
-form.addEventListener("submit", function(e) {
-    
+
+  let GENDER = document.querySelector('input[name="gender"]:checked').value;
+  let COURSES= document.querySelector('input[name="courses"]:checked').value;
+form.addEventListener("submit", function(e){
     e.preventDefault();
+    $('.message a').click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    })
+     });
+     
